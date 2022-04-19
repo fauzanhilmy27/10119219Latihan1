297b1937615058b70aaeb505352431c31c39f9b1
fauzanhilmy27 <filifauzanhilmy@gmail.com>
2022-04-19 22:35:17 +0700

    19-4-2022

---
+++ app/src/main/java/com/latihan1/a10119219latihan1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.latihan1.a10119219latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Nim   : 10119219
+//Nama  : Fauzan Hilmy
+//Kelas : IF-6
+//tanggal : 19-4-2022
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
